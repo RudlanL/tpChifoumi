b792a07e28bf6ecd74b494b06fe7ce9982869f7f
RudlanL <legrandkenny@gmail.com>
2022-05-19 16:44:21 +0200

    init: first commit

---
+++ src/main/java/fr/eni/tpChifoumi/TpChifoumiApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.tpChifoumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpChifoumiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpChifoumiApplication.class, args);
+	}
+
+}
